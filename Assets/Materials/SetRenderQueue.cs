239c904d9b8207f8a21e25b64d0a74aee18e6b4a
Michael Neises <neisesmichael@gmail.com>
2021-09-08 16:56:56 -0500

    final initial commit- mostly VR stuff

---
+++ Assets/Materials/SetRenderQueue.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[AddComponentMenu("Rendering/SetRenderQueue")]
+
+public class SetRenderQueue : MonoBehaviour
+{
+    [SerializeField]
+    protected int queueNum;
+
+    [SerializeField]
+    protected int[] m_queues = new int[] { 3000 };
+
+    protected void Update()
+    {
+        Material materials = GetComponent<Renderer>().material;
+        Debug.Log("setting material");
+        materials.renderQueue = queueNum; // m_queues[i];
+        //for ( int i=0; i<materials.Length && 1 < m_queues.Length; i++)
+        {
+        }
+    }
+}
---
+++ Assets/NewSurfaceShader.shader
@@ -0,0 +1,28 @@
+﻿Shader "Custom/NewSurfaceShader"
+{
+    Properties
+    {
+        //_Color ("Main Color", Color) = (1,.5,.5,1)
+        
+    }
+    SubShader
+    {
+		// a single pass
+		Pass
+		{
+			// use fixed function per-vertex lighting
+			Material
+			{
+				//Diffuse [_Color]
+			}
+			Lighting On
+		}
+		
+		Pass
+		{
+			ZTest Always
+		}
+        
+    }
+    FallBack "Diffuse"
+}
